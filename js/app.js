d2aa9145a62d09c29652d65fc0c19276c25d2a5d
deranimal <kenyamauchi@student.midpac.edu>
2016-03-25 09:39:38 -1000

    emotions
    this will bring out your inner emotions

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var music = new Audio("music/WeTheKings.mp3");
+
+var play = function () {
+	music.play();
+	console.log("we got here");
+}
+var music = new Audio("music/Sugar.mp3");
+
+var play = function () {
+	music.play();
+	console.log("we got here")
+}
